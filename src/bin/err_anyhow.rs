8aec391256c082b1cf53dcf050d9d764f2ebc247
yu2ta7ka <yuji.tanaak@gmail.com>
2021-03-17 15:40:02 +0900

    Use error crate.

---
+++ src/bin/err_anyhow.rs
@@ -0,0 +1,27 @@
+use anyhow::{bail, ensure, Context, Result};
+
+fn get_int_from_file() -> Result<i32> {
+    let path = "number.txt";
+
+    let num_str = std::fs::read_to_string(path)
+        .with_context(|| format!("failed to read string from {}", path))?;
+
+    if num_str.len() >= 10 {
+        bail!("it may be too large number");
+    }
+
+    ensure!(num_str.starts_with("1"), "first digit is not 1");
+
+    num_str
+        .trim()
+        .parse::<i32>()
+        .map(|t| t * 2)
+        .context("failed to parse string")
+}
+
+fn main() {
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{:#?}", e),
+    }
+}
